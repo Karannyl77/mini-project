c9e4040669897e5f4f3bfbd1a55c3a456d188d09
Karan Nayal <karannayal1234567@gmail.com>
2024-04-24 13:16:03 +0530

    final project

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const movieRouter = require("./routes/movie");
 require("dotenv").config();
 const app = express();
 
+
 // connection with local mongodb server
 // local mongodb url : mongodb://127.0.0.1:27017
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoose.connect('mongodb+srv://karannayal:imkaran@test-pro-db.st45ekg.mongodb.net/');
   console.log("database connected");
 }
 main().catch((err) => console.log(err));
@@ -22,12 +23,12 @@ app.use(cors());
 app.use("/api/booking", movieRouter);
 
 // Handeling Routes of Cliant
-app.use(express.static(path.resolve(__dirname, process.env.BUILD_DIR)));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, process.env.BUILD_DIR, "index.html"));
+  res.sendFile(path.join(__dirname, 'build', "index.html"));
 });
 
 // listening the server on port 8080.
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on the port ${process.env.PORT}`);
+app.listen(8080, () => {
+  console.log(`Server is running on the port 8080`);
 });
